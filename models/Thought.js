9b1675214eef80b7b0a6bbe4ccfbfe8905e1fb00
sacylkowski <sacylkowski@gmail.com>
2022-06-03 13:21:44 -0500

    add thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,66 @@
 // Reaction schema as a subdocument of Thought
+
+const { Schema, model, Types } = require("mongoose");
+
+const ReplySchema = new Schema({
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // get:
+    }
+},
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // get: 
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    });
+
+// get total count of reactions
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
